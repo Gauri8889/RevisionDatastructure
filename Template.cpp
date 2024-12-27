1e966cc04f1af49922e044a6715bb8eff85c7ca5
Gauri Dahake <dahakegauri4@gmail.com>
2024-12-27 22:04:06 +0530

    Template

---
+++ Template.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+template<typename data1,typename data2>
+data1 sum(data 1 a,data 2 b)
+{
+    return a+b;
+}
+int main()
+{
+    cout<<sum('!','@');//33+64
+    cout<<sum(12,7.1);
+}
