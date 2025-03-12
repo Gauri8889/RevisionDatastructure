f8dcd6a4715f493ff496c578b15709fed9ddde82
Gauri Dahake <dahakegauri4@gmail.com>
2025-03-12 21:00:51 +0530

    12March

---
+++ adition12.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=13;
+    cout<<a+b;
+}
---
+++ paturn3.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int rows;
+
+    cin >> rows;
+    for( int i = 0; i < rows; i++ ) {
+        int spaces = rows - i;
+        for( int j = 0; j < spaces; j++){
+            cout <<"  ";
+        }
+        for( int j = 0; j < 2 * i - 1; j++){
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
